585b140099225fed85948035b842fbdb3016f08c
Fadomire <fadomire@gmail.Com>
2012-12-12 13:44:03 +0100

    database config changes

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,12 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
-
-case Padrino.env
-  when :development then MongoMapper.database = 'augmented_development'
-  when :production  then MongoMapper.database = 'augmented_production'
-  when :test        then MongoMapper.database = 'augmented_test'
+if ENV['MONGOHQ_URL']
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'], :logger => logger)
+  MongoMapper.database = uri.path.gsub(/^\//, '')
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
+  case Padrino.env
+    when :development then MongoMapper.database = 'augmented_development'
+    when :production  then MongoMapper.database = 'augmented_production'
+    when :test        then MongoMapper.database = 'augmented_test'
+  end
 end
